eaa7dd23e0f717d68aaff8e1a262d930ebd906fd
aurora-to-arora <aurora.to.arora@gmail.com>
2025-11-17 13:56:07 +0530

    Create intToRoman.py

---
+++ intToRoman.py
@@ -0,0 +1,24 @@
+class Solution:
+    def intToRoman(self, n: int) -> str:
+        d = {
+            1000:'M',
+            900:'CM',
+            500:'D',
+            400:'CD',
+            100:'C',
+            90:'XC',
+            50:'L',
+            40:'XL',
+            10:'X',
+            9:'IX',
+            5:'V',
+            4:'IV',
+            1:'I'
+        }
+
+        res = ''
+        for i in d:              
+            while n >= i:
+                res += d[i]
+                n -= i
+        return res
