6aca479ebd4e7d253fb0a7aa22771f132664dbed
aurora-to-arora <aurora.to.arora@gmail.com>
2025-12-06 20:31:36 +0530

    Create hasPathSum.py

---
+++ hasPathSum.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+
+        stack = [(root, root.val)]
+
+        while stack:
+            node, num = stack.pop()
+
+            if not node.left and not node.right and num==targetSum:
+                return True
+            if node.right:
+                stack.append((node.right, num+node.right.val))
+            if node.left:
+                stack.append((node.left, num+node.left.val))
+        return False
+
