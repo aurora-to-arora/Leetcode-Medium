7bf702f334240e6675973d1876a7cb9a673ac67f
aurora-to-arora <aurora.to.arora@gmail.com>
2025-12-07 10:14:44 +0530

    Create intersectionOfList.py

---
+++ intersectionOfList.py
@@ -0,0 +1,11 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        n1=set(nums1)
+        n2 = set(nums2)
+        res=[]
+        for i in n1:
+            if i in n2:
+                res.append(i)
+
+        return res
+        #return list(set(nums1) & set(nums2))
